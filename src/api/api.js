b85b485fb542f769c0fc6af50c61944909df6d18
Vishhh2125 <vishnuuplenchwar.professional@gmail.com>
2025-06-05 03:10:44 +0530

    added render api link

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1/',
+  baseURL: 'https://project-2125-backend.onrender.com/api/v1/',
   withCredentials: true,
   validateStatus: (status) => status >= 200 && status < 300,
   headers: {
